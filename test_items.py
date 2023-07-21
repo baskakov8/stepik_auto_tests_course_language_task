95da451505b66fd2833beaa0fc8fa0762b6292f4
Andrei Baskakov <baskakov8@mail.ru>
2023-07-21 12:50:27 +0400

    Changed a type of exception to check the button

--- test_items.py
+++ test_items.py
@@ -1,9 +1,7 @@
 import pytest
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-import time
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
